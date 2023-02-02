ef454645d47312b436e97fdb2efd9df94b085d3e
fredyxander <freddy5210@gmail.com>
2023-02-02 17:43:02 -0500

    creacion del servidor

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`));
+
+
+app.get("/",(req,res)=>{
+    res.sendFile("index.html");
+});
