f587a2646a915dc193615dcd8fbd105a86c05713
fredyxander <freddy5210@gmail.com>
2023-02-02 17:50:39 -0500

    nueva ruta para obtener un usuario

--- src/server.js
+++ src/server.js
@@ -12,3 +12,7 @@ app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`));
 app.get("/",(req,res)=>{
     res.sendFile("index.html");
 });
+
+app.get("/user",(req,res)=>{
+    res.json({name:"fredy", email:"fredy@gmail.com"})
+});
